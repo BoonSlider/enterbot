fb49e5b356c9d4ae31af2257de74727818d7a9a9
a <a>
2025-02-26 00:29:05 +0200

    fix house name in profile

--- Player/House/HouseData.cs
+++ Player/House/HouseData.cs
@@ -7,4 +7,14 @@ public record HouseData
     public required long ProtectedGuards { get; init; }
     public required long MoonshineLimit { get; init; }
     public required long RequiredFame { get; init; }
+
+    public static string GetHouseName(long lvl)
+    {
+        return lvl switch
+        {
+            > 32 => $"Villa lvl {lvl - 32}",
+            > 16 => $"Maja lvl {lvl - 16}",
+            _ => $"Korter lvl {lvl}",
+        };
+    }
 }
